c5e6ca5e2fe8730ba51f34acf0c3c7adcf2b1b57
lolagracea <142556094+lolagracea@users.noreply.github.com>
2026-03-02 17:14:46 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -9,13 +9,16 @@ CREATE TABLE IF NOT EXISTS plants (
     updated_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS destinations (
-                                            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nama VARCHAR(150) NOT NULL,
-    negara VARCHAR(100) NOT NULL,
-    kota VARCHAR(100) NOT NULL,
-    path_gambar VARCHAR(255) NOT NULL,
-    deskripsi TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-    );
+
+CREATE TABLE destinations (
+                              id UUID PRIMARY KEY DEFAULT gen_random_uuid(),   -- sesuai UUID, bisa auto-generate
+
+                              name VARCHAR(100) NOT NULL,
+                              country VARCHAR(100) NOT NULL,
+                              city VARCHAR(100),                               -- nullable
+                              description TEXT,                                -- nullable
+                              path_gambar VARCHAR(255),                        -- nullable (pathGambar di Kotlin)
+
+                              created_at TIMESTAMPTZ NOT NULL DEFAULT now(),   -- Instant -> timestamp with time zone
+                              updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
